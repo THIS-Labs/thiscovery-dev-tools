eb84a568eb482bd115918e9efbec048fa7737a4c
Sophie Machin <sophie.machin@thisinstitute.cam.ac.uk>
2022-05-10 12:57:55 +0100

    Formatting

--- thiscovery_dev_tools/reinstall_requirements.py
+++ thiscovery_dev_tools/reinstall_requirements.py
@@ -3,23 +3,23 @@ import sys
 
 
 def uninstall(package):
-    print('pip uninstall ' + package)
+    print("pip uninstall " + package)
     subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "--yes", package])
 
 
 def install(package):
-    print('pip install ' + package)
+    print("pip install " + package)
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 if __name__ == "__main__":
 
-    with open('requirements.txt') as f:
+    with open("requirements.txt") as f:
         lines = f.readlines()
 
     for l in lines:
 
-        if l.startswith('https://github'):
+        if l.startswith("https://github"):
             uninstall(l.split("THIS-Institute/", 1)[1].split("/", 1)[0])
 
             install(l)
