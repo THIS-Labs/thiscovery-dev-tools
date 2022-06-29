bef919ba10a44775a401b27ffc5f73601f6759ba
André Sartori <afs25@cam.ac.uk>
2022-06-29 21:38:14 +0100

    #4633 comment: added fallback query routine to test_eb_request_v2

--- tests/unit/test_testing_tools.py
+++ tests/unit/test_testing_tools.py
@@ -22,29 +22,45 @@ import unittest
 from http import HTTPStatus
 
 
+@unittest.skipIf(
+    not test_tools.tests_running_on_aws(),
+    "Tests in this class check that test_eb_request_v2 works when running on AWS",
+)
 class EbRequestTestCase(test_tools.BaseTestCase):
-    @unittest.skipIf(
-        not test_tools.tests_running_on_aws(),
-        "This test only checks that test_eb_request_v2 works when running on AWS",
-    )
+    """
+    Tests in this class require a functional deployment of thiscovery-events
+    to the test environment because they rely on processing by the
+    PersistEvent AWS lambda
+    """
+
+    test_event = {
+        "detail-type": "test_event",
+        "detail": {
+            "appointment_id": 123456,
+            "user_id": "f2fac677-cb2c-42a0-9fa6-494059352569",
+        },
+    }
+
     def test_eb_request_v2_ok(self):
-        """
-        This test requires a functional deployment of thiscovery-events
-        to the test environment because it relies on processing by the
-        PersistEvent AWS lambda
-        """
-        test_event = {
-            "detail-type": "test_event",
-            "detail": {
-                "appointment_id": 123456,
-                "user_id": "f2fac677-cb2c-42a0-9fa6-494059352569",
-            },
-        }
         result = test_tools.test_eb_request_v2(
             local_method="Not applicable",
-            aws_eb_event=test_event,
+            aws_eb_event=self.test_event,
             lambda_name="PersistEvent",
             stack_name="thiscovery-events",
-            aws_processing_delay=30,
+            aws_processing_delay=5,
+        )
+        self.assertEqual(HTTPStatus.OK, result["statusCode"])
+
+    def test_eb_request_v2_query_fallback_ok(self):
+        """
+        This test takes a long time to complete.
+        """
+        result = test_tools.test_eb_request_v2(
+            local_method="Not applicable",
+            aws_eb_event=self.test_event,
+            lambda_name="PersistEvent",
+            stack_name="thiscovery-events",
+            aws_processing_delay=5,
+            force_query=True,
         )
         self.assertEqual(HTTPStatus.OK, result["statusCode"])
