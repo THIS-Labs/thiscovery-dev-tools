b55d23bf7dbb146552de8c16e1a3d4a61f9a2c83
Sophie Machin <sophie.machin@thislabs.org>
2024-08-12 12:32:42 +0100

    Add user verified event

--- thiscovery_dev_tools/test_data/auth0_events.py
+++ thiscovery_dev_tools/test_data/auth0_events.py
@@ -123,3 +123,50 @@ SUCCESSFUL_REGISTRATION = {
     "version": "0",
     "account": "REDACTED",
 }
+
+
+SUCCESSFUL_VERIFICATION = {
+    "detail-type": "Auth0 log",
+    "resources": [],
+    "id": "REDACTED",
+    "source": "REDACTED",
+    "time": "2021-01-16T12:12:43Z",
+    "detail": {
+        "data": {
+            "date": "2021-01-16T12:12:30.147Z",
+            "log_id": "90020220225104831160441788900872658485194909784700092514",
+            "user_name": "altha@email.co.uk",
+            "ip": "REDACTED",
+            "description": "",
+            "strategy_type": "database",
+            "type": "sv",
+            "client_id": "REDACTED",
+            "connection_id": "con_C72rtu1sAF9UFXmR",
+            "user_id": "REDACTED",
+            "connection": "Username-Password-Authentication",
+            "details": {
+                "body": {
+                    "password": "*****",
+                    "user_metadata": {
+                        "last_name": "Alcorn",
+                        "country": "GB",
+                        "first_name": "Altha",
+                        "citsci_uuid": "d1070e81-557e-40eb-a7ba-b951ddb7ebdc",
+                    },
+                    "name": "Altha Alcorn",
+                    "connection": "Username-Password-Authentication",
+                    "tenant": "thiscovery",
+                    "client_id": "REDACTED",
+                    "email": "altha@email.co.uk",
+                }
+            },
+            "strategy": "auth0",
+            "client_name": "Thiscovery",
+            "user_agent": "GuzzleHttp/6.3.3 curl/7.58.0 PHP/7.4.26",
+        },
+        "log_id": "90020220225104831160441788900872658485194909784700092514",
+    },
+    "region": "REDACTED",
+    "version": "0",
+    "account": "REDACTED",
+}
